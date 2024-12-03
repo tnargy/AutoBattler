aaee707a3953ba602e86632461712da18de1f4bb
tnargy-Desktop <clsscmstng@gmail.com>
2024-12-03 09:02:46 -0600

    Unit Spwaner

--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -4,3 +4,10 @@ extends Node2D
 const CELL_SIZE := Vector2(32, 32)
 const HALF_CELL_SIZE := Vector2(16, 16)
 const QUARTER_CELL_SIZE := Vector2(8, 8)
+
+@onready var unit_mover = $UnitMover
+@onready var unit_spawner = $UnitSpawner
+
+
+func _ready():
+    unit_spawner.unit_spawned.connect(unit_mover.setup_unit)
