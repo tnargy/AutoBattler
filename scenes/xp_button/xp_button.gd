4ce3479a18417cd0786e5d47520f6d2e26275e9a
tnargy-Desktop <clsscmstng@gmail.com>
2025-08-22 22:26:04 -0500

    refactored magic numbers

--- scenes/xp_button/xp_button.gd
+++ scenes/xp_button/xp_button.gd
@@ -14,10 +14,10 @@ func _ready():
 
 func _on_player_stats_changed():
 	var has_enough_gold := player_stats.gold >= 4
-	var level_10 := player_stats.level == 10
-	disabled = not has_enough_gold or level_10
+	var max_level := player_stats.level == PlayerStats.MAX_LEVEL
+	disabled = not has_enough_gold or max_level
 
-	if has_enough_gold and not level_10:
+	if has_enough_gold and not max_level:
 		vbox_container.modulate.a = 1.0
 	else:
 		vbox_container.modulate.a = 0.5
